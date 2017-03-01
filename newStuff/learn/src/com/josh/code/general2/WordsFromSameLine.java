a8056cc1cd0cb3ee65acdb5c15b789d8ed7d452a
joshimdr <munishjoshi@yahoo.com>
2017-02-28 21:55:05 -0800

    Words and zigzag printing

---
+++ newStuff/learn/src/com/josh/code/general2/WordsFromSameLine.java
@@ -0,0 +1,64 @@
+package com.josh.code.general2;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class WordsFromSameLine {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String[] result = findWords(new String[] { "WE", "WEIGH", "BXCVN" });
+
+		for (String w : result) {
+			System.out.println(w);
+		}
+		
+		System.out.println("Test comment");
+
+	}
+
+	public static String[] findWords(String[] words) {
+		String[] strs = { "QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM" };
+
+		Map wordMap = new HashMap();
+		// int i=0;
+
+		for (int i = 0; i < strs.length; i++) {
+			// for(String wrd : strs){
+			for (char charac : strs[i].toCharArray()) {
+				wordMap.put(charac, i);
+			}
+
+		}
+
+		if (words == null || words.length < 1) {
+			return null;
+		}
+
+		List<String> res = new LinkedList<>();
+
+		for (String w : words) {
+
+			int in = (int) wordMap.get(w.toUpperCase().charAt(0));
+
+			for (char c : w.toUpperCase().toCharArray()) {
+
+				if ((int) wordMap.get(c) != in) {
+					in = -1;
+					break;
+
+				}
+			}
+			if (in != -1) {
+				res.add(w);
+			}
+
+		}
+
+		return res.toArray(new String[0]);
+	}
+
+}
