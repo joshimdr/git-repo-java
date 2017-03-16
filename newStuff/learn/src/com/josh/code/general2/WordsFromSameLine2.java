00a67dab79009e76fea7299e1eb6afb098013b3a
joshimunish <Munish@dn0a22f95c.sunet>
2017-03-16 11:08:06 -0700

    Name Change of the class

--- newStuff/learn/src/com/josh/code/general2/WordsFromSameLine2.java
+++ newStuff/learn/src/com/josh/code/general2/WordsFromSameLine2.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-public class WorksFromSameLine {
+public class WordsFromSameLine2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
